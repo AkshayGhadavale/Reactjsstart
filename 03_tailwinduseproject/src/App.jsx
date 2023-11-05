7a12ab3088f512b1713c99dfc964ae9544900c09
AkshayGhadavale <akshayghadavale25@gmail.com>
2023-11-05 17:49:46 +0530

    third peoject props

---
+++ 03_tailwinduseproject/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Card1 from './Components/Card1'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    
+  <Card1 username="akshay"></Card1>
+  <Card1 username="Ghadavele"></Card1>
+    </>
+  )
+}
+
+export default App
